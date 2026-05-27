74baa5ba047e0719cf88dc568a0eefccacb774f4
Stephanie Owen <1626609313113305@mil>
2026-05-27 11:29:24 -0400

    run plaice bt data

--- data-raw/scripts/spatial_code_template.R
+++ data-raw/scripts/spatial_code_template.R
@@ -55,53 +55,54 @@ write.csv(
   row.names = FALSE
 )
 
-# glorys bottom salinity ----
-glorys_species_sal <- NEesp2::create_spatial_indicator(
-  indicator_name = "bottomS",
-  units = "1e-3",
-  data.in = glorys_salinity,
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', '{{ species }}_glorys_bottomS.nc')),
-  shp.file = species_shp,
-  var.name = "so",
-  area.names = "stock_area",
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
+# # glorys bottom salinity ----
+# glorys_species_sal <- NEesp2::create_spatial_indicator(
+#   indicator_name = "bottomS",
+#   units = "1e-3",
+#   data.in = glorys_salinity,
+#   file.time = 'annual',
+#   output.files = c(here::here('data-raw', '{{ species }}_glorys_bottomS.nc')),
+#   shp.file = species_shp,
+#   var.name = "so",
+#   area.names = "stock_area",
+#   statistic = 'mean',
+#   agg.time = 'days',
+#   tz = NA,
+#   touches = TRUE,
+#   write.out = F
+# )
+# 
+# write.csv(
+#   glorys_species_sal,
+#   here::here('data-raw/2026', '{{ species }}_glorys_bottomS.csv'),
+#   row.names = FALSE
+# )
+# 
+# # chlorophyll ----
+# 
+# occci_chl <- NEesp2::create_spatial_indicator(
+#   indicator_name = "chlor_a",
+#   units = "m^-3",
+#   data.in = chl,
+#   file.time = 'annual',
+#   output.files = c(here::here('data-raw', '{{ species }}_chl.nc')),
+#   shp.file = species_shp,
+#   var.name = "chlor_a",
+#   area.names = "stock_area",
+#   statistic = 'mean',
+#   agg.time = 'days',
+#   tz = NA,
+#   touches = TRUE,
+#   write.out = F
+# )
+# 
+# write.csv(
+#   occci_chl,
+#   here::here('data-raw/2026', '{{ species }}_chl.csv'),
+#   row.names = FALSE
+# )
 
-write.csv(
-  glorys_species_sal,
-  here::here('data-raw/2026', '{{ species }}_glorys_bottomS.csv'),
-  row.names = FALSE
-)
-
-# chlorophyll ----
-
-occci_chl <- NEesp2::create_spatial_indicator(
-  indicator_name = "chlor_a",
-  units = "m^-3",
-  data.in = chl,
-  file.time = 'annual',
-  output.files = c(here::here('data-raw', '{{ species }}_chl.nc')),
-  shp.file = species_shp,
-  var.name = "chlor_a",
-  area.names = "stock_area",
-  statistic = 'mean',
-  agg.time = 'days',
-  tz = NA,
-  touches = TRUE,
-  write.out = F
-)
-
-write.csv(
-  occci_chl,
-  here::here('data-raw/2026', '{{ species }}_chl.csv'),
-  row.names = FALSE
-)
-
+############################################################
 # map ----
 # us <- geodata::gadm(
 #   country = "USA",
